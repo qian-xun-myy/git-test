42d2ba6d12cf096f7a4809fc8a3f6e3a6eef5521
Daisy <1304055692@qq.com>
2022-11-09 22:43:50 +0800

    v1

---
+++ src/main/java/com/myy/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.myy.git;
+
+/**
+ * @author myy
+ * @version 1.0
+ * @Description
+ * @date 2022/11/9 22:11
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
